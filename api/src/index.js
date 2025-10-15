381875af13d865bbc287e5e4d52e55916fcf5d80
Ivaylo Papazov <papazov.ivaylo@gmail.com>
2025-10-15 18:47:45 +0300

    Add express to api

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,11 @@
-console.log('It Works');
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    console.log('It Works');
+    res.send('It Works!');
+});
+
+app.listen(3030, () => 'Server is listening on http://localhost:3030...');
+
